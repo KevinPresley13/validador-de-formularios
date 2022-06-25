d17ee71b6dac3b7ecbc7900a967e5f195ed51da6
Kevin Presley <kevinpresley1995@gmail.com>
2022-06-25 16:40:13 -0300

    funçao de erro

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let validator ={
     handleSubmit:(event)=>{
         event.preventDefault();
 
+        validator.clearError();
         let send = true;
         let inputs = document.querySelectorAll('input');
         for(let i=0; i<inputs.length; i++){
@@ -11,8 +12,8 @@ let validator ={
             let check = validator.checkInput(input);
             if(check !==true){
                 send = false;
-                console.log(check)
-            };
+                validator.showError(input,check);
+            }
         };
 
         if(send){
@@ -41,6 +42,25 @@ let validator ={
 
         }
         return true;
+    },
+    showError:(input,error)=>{
+        input.style.borderColor ='#ff0000';
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error')
+        errorElement.innerHTML = error
+
+        input.parentElement.insertBefore(errorElement,input.ElementSibling);
+    },
+    clearError:(input)=>{
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0; i<errorElements.length; i++){
+            let inputs = form.querySelectorAll('input');
+            for (let i=0; i<inputs.length; i++){
+                inputs[i].style = '';
+            }
+            errorElements[i].remove();
+            
+        }
     }
 };
 let form = document.querySelector('form');
